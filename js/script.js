c8d78005b1c9299aac45e9fedece965dab548720
devfelipelimabr <felipelima85@hotmail.com>
2022-09-23 17:59:35 -0300

    update design and substituted banners of the slide

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function search(){
+    const find = document.getElementById('searchInput');
+
+
+    find.innerHTML = `<input type="text" id="myInput" placeholder="Digite o nome e pressione enter..." style="display: block;">`;
+}
